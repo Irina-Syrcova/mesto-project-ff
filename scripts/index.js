43db29dda7e79d7ae32686dce484a6fee968b0cf
Irina Syrcova <syrtcova.ir@yandex.ru>
2024-02-02 20:51:05 +0300

    исправления

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,40 @@
+// const placesList = document.querySelector('.places__list');
+// const cardTemplate = document.querySelector('#card-template').content;
+
+// initialCards.forEach(function(element){
+//   const cardElement = cardTemplate.cloneNode(true);
+//   cardElement.querySelector('.card__image').src = element.link;
+//   cardElement.querySelector('.card__title').textContent = element.name;
+
+//   const deleteButton = cardElement.querySelector('.card__delete-button');
+//   deleteButton.addEventListener('click', function () {
+//   const listItem = deleteButton.closest('.places__item');
+//   listItem.remove();
+//   });
+
+//   placesList.append(cardElement);
+// });
 const placesList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 
-initialCards.forEach(function(element){
+function createCard(cardData, deleteCallback) {
   const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelector('.card__title').textContent = element.name;
-
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', function () {
-  const listItem = deleteButton.closest('.places__item');
-  listItem.remove();
-  });
 
-  placesList.append(cardElement);
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+};
+
+function deleteCard(event) {
+  const listItem = event.target.closest('.places__item');
+  listItem.remove();
+}
+
+initialCards.forEach(function(element) {
+  placesList.append(createCard(element, deleteCard));
 });
