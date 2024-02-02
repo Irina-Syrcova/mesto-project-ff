d1777a8032bd3d0af9a01cd16ce9f9f6a021304e
Irina Syrcova <syrtcova.ir@yandex.ru>
2024-02-02 17:51:28 +0300

    первый вариант

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+initialCards.forEach(function(element){
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
 
-// @todo: Функция создания карточки
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function () {
+  const listItem = deleteButton.closest('.places__item');
+  listItem.remove();
+  });
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+  placesList.append(cardElement);
+});
